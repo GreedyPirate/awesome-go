f820a16c462d8076802e8e1ca3862ed1eb4184b1
GreedyPirate <18335166203@163.com>
2020-08-18 14:39:42 +0800

    update

--- src/day3/fun_test.go
+++ src/day3/fun_test.go
@@ -5,3 +5,12 @@ import "testing"
 func TestFunction(t *testing.T) {
 
 }
+
+// 类型一致的形参可以这样写
+func typeful(a, b int, s1, s2 string)  {
+	print(a, b, s1, s2)
+}
+
+func multi() (int, int)  {
+	return 1,2
+}
